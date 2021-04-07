671cecec86a7b82719c424cdf1cf87c46f00ce5f
Brieuc-brd <brd@odoo.com>
2021-04-07 17:00:00 +0200

    [ADD] theme_buzzy : add new theme
    Add new corporate theme, with illustrations and shapes,
    for the web builder.
    Task-2582420

---
+++ theme_buzzy/static/src/js/tour.js
@@ -0,0 +1,48 @@
+odoo.define("theme_buzzy.tour.buzzy", function (require) {
+"use strict";
+
+const wTourUtils = require("website.tour_utils");
+var tour = require("web_tour.tour");
+
+const snippets = [
+    {
+        id: 's_banner',
+        name: 'Banner',
+    },
+    {
+        id: 's_text_image',
+        name: 'Text - Image',
+    },
+    {
+        id: 's_three_columns',
+        name: 'Columns',
+    },
+    {
+        id: 's_image_text',
+        name: 'Image - Text',
+    },
+    {
+        id: 's_numbers',
+        name: 'Numbers',
+    },
+    {
+        id: 's_call_to_action',
+        name: 'Call to Action',
+    },
+];
+
+wTourUtils.registerThemeHomepageTour("buzzy_tour", [
+    wTourUtils.dragNDrop(snippets[0]),
+    wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
+    wTourUtils.goBackToBlocks(),
+    wTourUtils.dragNDrop(snippets[1]),
+    wTourUtils.dragNDrop(snippets[2]),
+    wTourUtils.clickOnSnippet(snippets[2], 'top'),
+    wTourUtils.changeBackgroundColor(),
+    wTourUtils.selectColorPalette(),
+    wTourUtils.goBackToBlocks(),
+    wTourUtils.dragNDrop(snippets[3]),
+    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.dragNDrop(snippets[5]),
+]);
+});
