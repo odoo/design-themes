336d7d9fe26166205c1e9f99df3dc6746cb93a4d
qsm-odoo <qsm@odoo.com>
2021-05-12 10:22:32 +0000

    [IMP] test_themes: add a test which goes over all theme tours
    Part of https://github.com/odoo/design-themes/pull/2
    task-2491890
    
    X-original-commit: 52d224fd9b60ad034197bbb49b5e5d44c1e5fe52

--- test_themes/tests/test_crawl.py
+++ test_themes/tests/test_crawl.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import unittest
+
 from odoo.tests import HttpCase, tagged
 
 
@@ -37,3 +39,11 @@ class Crawler(HttpCase):
         # 2. Test as admin
         self.authenticate('admin', 'admin')
         test_crawling()
+
+    # Does not work without editor fix but really useful to build the pages automatically with cr.commit()
+    @unittest.skip
+    def test_02_homepage_tour_every_theme(self):
+        Website = self.env['website']
+        websites_themes = Website.get_test_themes_websites()
+        for website in websites_themes:
+            self.start_tour(f"/?fw={website.id}", 'homepage', login='admin')
