18d1f0a532ab7a954ce0ffbe2efc0605033a2476
xlu-odoo <xlu@odoo.com>
2021-05-28 08:06:34 +0000

    [ADD] theme_aviato: add new theme aviato
    task-2589967
    
    Part-of: odoo/design-themes#500

---
+++ theme_aviato/static/src/js/tour.js
@@ -0,0 +1,47 @@
+/** @odoo-module */
+
+import wTourUtils from 'website.tour_utils';
+
+const core = require("web.core");
+const _t = core._t;
+
+const snippets = [
+    {
+        id: 's_cover',
+        name: 'Cover',
+    },
+    {
+        id: 's_text_image',
+        name: 'Text - Image',
+    },
+    {
+        id: 's_image_text',
+        name: 'Image - Text',
+    },
+    {
+        id: 's_title',
+        name: 'Title',
+    },
+    {
+        id: 's_three_columns',
+        name: 'Columns',
+    },
+    {
+        id: 's_picture',
+        name: 'Picture',
+    },
+];
+
+wTourUtils.registerThemeHomepageTour("aviato_tour", [
+    wTourUtils.dragNDrop(snippets[0]),
+    wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
+    wTourUtils.goBackToBlocks(),
+    wTourUtils.dragNDrop(snippets[1]),
+    wTourUtils.dragNDrop(snippets[2]),
+    wTourUtils.dragNDrop(snippets[3]),
+    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.dragNDrop(snippets[5]),
+    wTourUtils.clickOnSnippet(snippets[5], 'top'),
+    wTourUtils.changeOption('BackgroundShape', 'we-toggler', _t('Background Shape')),
+    wTourUtils.selectNested('we-select-page', 'BackgroundShape', ':not(.o_we_pager_controls)', _t('Background Shape')),
+]);
