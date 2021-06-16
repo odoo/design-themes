7411ad6b49e624384a742d290dcf0cdacc617de6
Carlos Valverde <cvs@odoo.com>
2021-06-16 09:22:48 +0000

    [IMP] theme_kiddo: update the theme
    Theme Kiddo's design has been revamped by adding the following
    improvements:
    
    - Theme palette has been changed, using less contrasted colours.
    - Fonts have been reviewed, replacing 'Bubblegum Sans' with 'Varela
    Round'.
    - Header and footer default values have been reviewed and changed to
    'default' and 'descriptive'.
    - Buttons' default design has been revamped, using now outline buttons
    by default.
    - Theme's tour has been reviewed and changed, using 's_picture' as a
    third block.
    - Theme's screenshot and description have been updated.
    - Theme's tour snippet design has been revamped, by using animated
    shapes ('s_banner', 's_picture'), new shapes ('s_image_text',
    's_call_to_action'), a bg video + img placeholder ('s_banner'), a
    illustration with dynamic colours ('s_image_text'), a new copywriting
    ('s_picture'), better text block design ('s_banner'), better design in
    smaller viewports ('s_banner', 's_product_list') and a brand new
    snippet design ('s_product_list').
    - In order to add a better integration with the Website Configurator,
    's_cover' design has also been improved.
    
    Additionally, duplicated commented text lines in
    'bootstrap_overridden.scss' have been removed.
    
    task-2573229
    
    X-original-commit: 0173ac6c44321837940212828a87dfe422fd2daa
    Part-of: odoo/design-themes#501

--- theme_kiddo/models/theme_kiddo.py
+++ theme_kiddo/models/theme_kiddo.py
@@ -5,11 +5,10 @@ class ThemeKiddo(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_kiddo_post_copy(self, mod):
-        self.disable_view('website.template_header_default')
-        self.enable_view('website.template_header_centered_logo')
+        self.enable_view('website.template_header_default_align_right')
 
         self.disable_view('website.footer_custom')
-        self.enable_view('website.template_footer_minimalist')
+        self.enable_view('website.template_footer_descriptive')
 
         # For compatibility
         # self.enable_asset('theme_common.compatibility_saas_10_1')
