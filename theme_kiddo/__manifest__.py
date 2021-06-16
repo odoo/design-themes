0eb2e496a4ed7ac678f5be6ad876e4e2a5781970
Carlos Valverde <cvs@odoo.com>
2021-06-16 11:22:48 +0200

    [IMP] theme_kiddo: theme design improvement
    Theme Kiddo's design has been revamped by adding the following
    improvements:
    
    - Theme palette has been changed, using less contrasted colours.
    - Fonts have been reviewed, replacing 'Bubblegum Sans' with 'Varela
    Round'.
    - Header and footer default values have been reviewed and changed to
    'default' and 'descriptive'.
    - Buttons' default design has been revamped, using now outline buttons
    by default.
    - Theme's tour has been reviewed and changed, using 's_picture' as a
    third block.
    - Theme's screenshot and description have been updated.
    - Theme's tour snippet design has been revamped, by using animated
    shapes ('s_banner', 's_picture'), new shapes ('s_image_text',
    's_call_to_action'), a bg video + img placeholder ('s_banner'), a
    illustration with dynamic colours ('s_image_text'), a new copywriting
    ('s_picture'), better text block design ('s_banner'), better design in
    smaller viewports ('s_banner', 's_product_list') and a brand new
    snippet design ('s_product_list').
    - In order to add a better integration with the Website Configurator,
    's_cover' design has also been improved.
    
    Additionally, duplicated commented text lines in
    'bootstrap_overridden.scss' have been removed.
    
    --
    
    Task-2573229

--- theme_kiddo/__manifest__.py
+++ theme_kiddo/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Theme/Retail',
     'summary': 'Nursery, Toys, Games, Kids, Boys, Girls, Stores',
     'sequence': 290,
-    'version': '2.0.0',
+    'version': '2.1.0',
     'author': 'Odoo S.A.',
     'depends': ['theme_common'],
     'data': [
@@ -13,9 +13,10 @@
 
         'views/snippets/s_banner.xml',
         'views/snippets/s_image_text.xml',
+        'views/snippets/s_picture.xml',
         'views/snippets/s_product_list.xml',
-        'views/snippets/s_three_columns.xml',
         'views/snippets/s_call_to_action.xml',
+        'views/snippets/s_cover.xml',
 
     ],
     'images': [
@@ -23,17 +24,8 @@
         'static/description/kiddo_screenshot.jpg',
     ],
     'images_preview_theme': {
-        'website.s_banner_default_image': '/theme_kiddo/static/src/img/library/bg11.jpg',
-        'website.s_image_text_default_image': '/theme_kiddo/static/src/img/content/content_img_14.jpg',
-        'website.s_product_list_default_image_1': '/theme_kiddo/static/src/img/content/content_img_30.jpg',
-        'website.s_product_list_default_image_2': '/theme_kiddo/static/src/img/content/content_img_31.jpg',
-        'website.s_product_list_default_image_3': '/theme_kiddo/static/src/img/content/content_img_32.jpg',
-        'website.s_product_list_default_image_4': '/theme_kiddo/static/src/img/content/content_img_33.jpg',
-        'website.s_product_list_default_image_5': '/theme_kiddo/static/src/img/content/content_img_34.jpg',
-        'website.s_product_list_default_image_6': '/theme_kiddo/static/src/img/content/content_img_35.jpg',
-        's_three_columns_default_image_1': '/theme_kiddo/static/src/img/content/content_img_16.jpg',
-        's_three_columns_default_image_2': '/theme_kiddo/static/src/img/content/content_img_17.jpg',
-        's_three_columns_default_image_3': '/theme_kiddo/static/src/img/content/content_img_18.jpg',
+        'website.s_banner_default_image': '/theme_kiddo/static/src/img/snippets/s_banner.jpg',
+        'website.s_picture_default_image': '/theme_kiddo/static/src/img/content/content_img_15.jpg',
     },
     'license': 'LGPL-3',
     'live_test_url': 'https://theme-kiddo.odoo.com',
