723a5813026bdba0155cd171acad3a2fbbedabb5
xlu-odoo <xlu@odoo.com>
2021-06-24 16:55:03 +0200

    [IMP] theme_real_estate: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2551125
    
    X-original-commit: 42638dcf22112b9c9d16a5d7d369ba4deac5ad2a

--- theme_real_estate/models/theme_real_estate.py
+++ theme_real_estate/models/theme_real_estate.py
@@ -8,12 +8,5 @@ class ThemeRealEstate(models.AbstractModel):
         # For compatibility
         # self.enable_asset('theme_common.compatibility_saas_10_2')
 
-        self.disable_view('website.template_header_default')
-        self.enable_view('website.template_header_hamburger')
-        self.enable_header_off_canvas()
-
-        self.disable_view('website.footer_custom')
-        self.enable_view('website.template_footer_descriptive')
-
         self.enable_asset('website.ripple_effect_scss')
         self.enable_asset('website.ripple_effect_js')
