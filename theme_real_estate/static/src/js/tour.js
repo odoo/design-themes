42638dcf22112b9c9d16a5d7d369ba4deac5ad2a
xlu-odoo <xlu@odoo.com>
2021-06-24 14:55:03 +0000

    [IMP] theme_real_estate: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2551125
    
    Part-of: odoo/design-themes#500

--- theme_real_estate/static/src/js/tour.js
+++ theme_real_estate/static/src/js/tour.js
@@ -10,16 +10,36 @@ const snippets = [
         name: 'Banner',
     },
     {
-        id: 's_picture',
-        name: 'Picture',
+        id: 's_text_block',
+        name: 'Text',
+    },
+    {
+        id: 's_text_image',
+        name: 'Text - Image',
     },
     {
         id: 's_image_text',
         name: 'Image - Text',
     },
     {
+        id: 's_title',
+        name: 'Title',
+    },
+    {
         id: 's_three_columns',
         name: 'Columns',
+    },
+    {
+        id: 's_title:last-child',
+        name: 'Title',
+    },
+    {
+        id: 's_masonry_block',
+        name: 'Masonry',
+    },
+    {
+        id: 's_numbers',
+        name: 'Numbers',
     },
     {
         id: 's_quotes_carousel',
@@ -34,10 +54,15 @@ wTourUtils.registerThemeHomepageTour("real_estate_tour", [
     wTourUtils.dragNDrop(snippets[1]),
     wTourUtils.dragNDrop(snippets[2]),
     wTourUtils.dragNDrop(snippets[3]),
-    wTourUtils.clickOnSnippet(snippets[3]),
+    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.clickOnSnippet(snippets[4]),
     wTourUtils.changeBackgroundColor(),
     wTourUtils.selectColorPalette(),
     wTourUtils.goBackToBlocks(),
-    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.dragNDrop(snippets[5]),
+    wTourUtils.dragNDrop(snippets[6]),
+    wTourUtils.dragNDrop(snippets[7]),
+    wTourUtils.dragNDrop(snippets[8]),
+    wTourUtils.dragNDrop(snippets[9]),
 ]);
 });
