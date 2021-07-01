16e708bb7ba8e48ff9ee964e33e59db067796d87
Brieuc-brd <brd@odoo.com>
2021-07-01 17:08:23 +0200

    [IMP] theme_odoo_experts: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2585389
    
    X-original-commit: b2ab9eadd597759e7ab7c18b86c32222dec895de

--- theme_odoo_experts/static/src/js/tour.js
+++ theme_odoo_experts/static/src/js/tour.js
@@ -1,15 +1,14 @@
-odoo.define("theme_odoo_experts.tour.odoo_experts", function (require) {
-"use strict";
+/** @odoo-module */
+
+import wTourUtils from 'website.tour_utils';
 
 const core = require("web.core");
 const _t = core._t;
-const wTourUtils = require("website.tour_utils");
-var tour = require("web_tour.tour");
 
 const snippets = [
     {
-        id: 's_banner',
-        name: 'Banner',
+        id: 's_cover',
+        name: 'Cover',
     },
     {
         id: 's_image_text',
@@ -18,6 +17,14 @@ const snippets = [
     {
         id: 's_media_list',
         name: 'Media List',
+    },
+    {
+        id: 's_company_team',
+        name: 'Team',
+    },
+    {
+        id: 's_references',
+        name: 'References',
     },
     {
         id: 's_call_to_action',
@@ -32,7 +39,9 @@ wTourUtils.registerThemeHomepageTour("odoo_experts_tour", [
     wTourUtils.dragNDrop(snippets[1]),
     wTourUtils.dragNDrop(snippets[2]),
     wTourUtils.dragNDrop(snippets[3]),
-    wTourUtils.clickOnSnippet(snippets[3]),
-    wTourUtils.changeOption('ScrollButton', 'we-button-group.o_we_user_value_widget', _t('height')),
+    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.clickOnSnippet(snippets[4], 'top'),
+    wTourUtils.changeOption('ContainerWidth', 'we-button-group.o_we_user_value_widget', _t('width')),
+    wTourUtils.goBackToBlocks(),
+    wTourUtils.dragNDrop(snippets[5]),
 ]);
-});
