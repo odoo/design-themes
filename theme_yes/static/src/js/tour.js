2a2da472705602c3c83f7f8cbb0501148b04fac2
Elisabeth Dickinson <edi@odoo.com>
2021-07-01 14:16:13 +0000

    [IMP] theme_yes: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2555035
    
    Part-of: odoo/design-themes#500

--- theme_yes/static/src/js/tour.js
+++ theme_yes/static/src/js/tour.js
@@ -1,48 +1,41 @@
-odoo.define("theme_yes.tour.yes", function (require) {
-"use strict";
+/** @odoo-module */
 
-const core = require("web.core");
-const _t = core._t;
-const wTourUtils = require("website.tour_utils");
-var tour = require("web_tour.tour");
+import wTourUtils from 'website.tour_utils';
 
 const snippets = [
     {
-        id: 's_carousel',
-        name: 'Carousel',
+        id: 's_cover',
+        name: 'Cover',
     },
     {
-        id: 's_picture',
-        name: 'Picture',
+        id: 's_title',
+        name: 'Title',
     },
     {
-        id: 's_masonry_block',
-        name: 'Masonry',
+        id: 's_company_team',
+        name: 'Team',
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
+        id: 's_media_list',
+        name: 'Media List',
+    },
+    {
+        id: 's_images_wall',
+        name: 'Images Wall',
     },
     {
         id: 's_quotes_carousel',
         name: 'Quotes',
     },
-    {
-        id: 's_call_to_action',
-        name: 'Call to Action',
-    },
 ];
 
 wTourUtils.registerThemeHomepageTour("yes_tour", [
     wTourUtils.dragNDrop(snippets[0]),
-    wTourUtils.clickOnText(snippets[0], 'h2'),
-    wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[1]),
+    wTourUtils.clickOnText(snippets[1], 'h1'),
+    wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[2]),
     wTourUtils.dragNDrop(snippets[3]),
     wTourUtils.dragNDrop(snippets[4]),
     wTourUtils.dragNDrop(snippets[5]),
-    wTourUtils.clickOnSnippet(snippets[5]),
-    wTourUtils.changeOption('ScrollButton', 'we-button-group.o_we_user_value_widget', _t('height')),
 ]);
-});
