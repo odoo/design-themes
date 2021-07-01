0be200d08cd9cb0250bec3ba1515023337868de5
Elisabeth Dickinson <edi@odoo.com>
2021-07-01 16:16:13 +0200

    [IMP] theme_yes: improve snippet customizations and theme presets
    Update the theme to improve the design
    with shapes, new colors, new pictures and new custom snippets.
    The tour has also been adapted.
    
    task-2555035

--- theme_yes/models/theme_yes.py
+++ theme_yes/models/theme_yes.py
@@ -5,8 +5,5 @@ class ThemeYes(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_yes_post_copy(self, mod):
-
-        self.disable_view('website.template_footer_call_to_action')
-
-        self.enable_view('website.template_header_default')
+        self.disable_view('website.footer_custom')
         self.enable_view('website.template_footer_descriptive')
