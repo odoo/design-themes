effa80b59c842f75d304a3c1fa9fe1286d0bbd80
Brieuc-brd <brd@odoo.com>
2021-07-06 16:56:59 +0200

    [IMP] theme_notes: update the theme
    This commit updates the theme to improve the design with shapes, new
    pictures and new custom snippets.
    
    task-2590454

--- theme_notes/models/theme_notes.py
+++ theme_notes/models/theme_notes.py
@@ -5,9 +5,6 @@ class ThemeNotes(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_notes_post_copy(self, mod):
-        self.disable_view('website.template_header_default')
-        self.enable_view('website.template_header_hamburger_full')
-        self.enable_view('website.template_header_hamburger_align_right')
         self.enable_header_off_canvas()
 
         self.disable_view('website.footer_custom')
