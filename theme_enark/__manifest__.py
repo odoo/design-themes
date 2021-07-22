38f0ffb41d8cb3fe0e46fc63cd691f4946977efe
xlu-odoo <xlu@odoo.com>
2021-07-22 16:33:03 +0200

    [IMP] theme_enark: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2601581
    
    X-original-commit: 3f75d0377bc195b3705710faa98e981c0d0306ff

--- theme_enark/__manifest__.py
+++ theme_enark/__manifest__.py
@@ -13,16 +13,31 @@
         'views/image_library.xml',
 
         'views/snippets/s_banner.xml',
+        'views/snippets/s_cover.xml',
         'views/snippets/s_text_image.xml',
         'views/snippets/s_picture.xml',
         'views/snippets/s_media_list.xml',
         'views/snippets/s_call_to_action.xml',
+        'views/snippets/s_parallax.xml',
+        'views/snippets/s_numbers.xml',
+        'views/snippets/s_image_gallery.xml',
 
         ],
     'images': [
         'static/description/enark_description.jpg',
         'static/description/enark_screenshot.jpg',
     ],
+    'images_preview_theme': {
+        'website.s_banner_default_image': '/theme_enark/static/src/img/snippets/s_banner.jpg',
+        'website.s_picture_default_image': '/theme_enark/static/src/img/snippets/s_picture.jpg',
+        'website.s_text_image_default_image': '/theme_enark/static/src/img/snippets/s_text_image.jpg',
+        'website.library_image_10': '/theme_enark/static/src/img/snippets/library_image_03.jpg',
+        'website.library_image_05': '/theme_enark/static/src/img/snippets/library_image_15.jpg',
+        'website.library_image_16': '/theme_enark/static/src/img/snippets/library_image_14.jpg',
+        'website.library_image_13': '/theme_enark/static/src/img/snippets/library_image_10.jpg',
+        'website.library_image_08': '/theme_enark/static/src/img/snippets/library_image_05.jpg',
+        'website.library_image_02': '/theme_enark/static/src/img/snippets/library_image_16.jpg',
+    },
     'license': 'LGPL-3',
     'live_test_url': 'https://theme-enark.odoo.com',
     'assets': {
