d13ea2568841abd9a23690886c17bfca03f0d851
Cocographique <nmi@odoo.com>
2021-08-01 23:13:17 +0200

    [IMP] theme_nano: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2590182
    
    X-original-commit: 036df33043a57577316a505918d244cd208378ba

--- theme_nano/static/src/js/tour.js
+++ theme_nano/static/src/js/tour.js
@@ -1,42 +1,45 @@
-odoo.define("theme_nano.tour.nano", function (require) {
-"use strict";
+/** @odoo-module */
 
-const wTourUtils = require("website.tour_utils");
-var tour = require("web_tour.tour");
+import wTourUtils from 'website.tour_utils';
 
 const snippets = [
     {
-        id: 's_carousel',
-        name: 'Carousel',
+        id: 's_cover',
+        name: 'Cover',
     },
     {
         id: 's_features',
         name: 'Features',
     },
     {
-        id: 's_image_text',
-        name: 'Image - Text',
-    },
-    {
         id: 's_text_block',
         name: 'Text',
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
+        id: 's_images_wall',
+        name: 'Images Wall',
+    },
+    {
+        id: 's_parallax',
+        name: 'Parallax',
+    },
+    {
+        id: 's_references',
+        name: 'References',
     },
 ];
 
 wTourUtils.registerThemeHomepageTour("nano_tour", [
     wTourUtils.dragNDrop(snippets[0]),
-    wTourUtils.clickOnText(snippets[0], 'h2', 'top'),
+    wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[1]),
     wTourUtils.dragNDrop(snippets[2]),
-    wTourUtils.dragNDrop(snippets[3]),
-    wTourUtils.dragNDrop(snippets[4]),
-    wTourUtils.clickOnSnippet(snippets[4], 'top'),
+    wTourUtils.clickOnSnippet(snippets[2], 'top'),
     wTourUtils.changeBackgroundColor(),
     wTourUtils.selectColorPalette(),
+    wTourUtils.goBackToBlocks(),
+    wTourUtils.dragNDrop(snippets[3]),
+    wTourUtils.dragNDrop(snippets[4]),
+    wTourUtils.dragNDrop(snippets[5]),
 ]);
-});
