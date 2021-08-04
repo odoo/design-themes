7b7b3bff968135481028831c23ec2dfa99259203
Brieuc-brd <brd@odoo.com>
2021-08-04 16:38:09 +0200

    [IMP] theme_anelusia: update the theme
    This commit updates the theme to improve the design with shapes, new
    colors, new pictures and new custom snippets.
    
    task-2573284

--- theme_anelusia/models/theme_anelusia.py
+++ theme_anelusia/models/theme_anelusia.py
@@ -5,10 +5,6 @@ class ThemeAnelusia(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_anelusia_post_copy(self, mod):
-        self.disable_view('website.template_header_default')
-        self.enable_view('website.template_header_hamburger_full')
-        self.enable_view('website.template_header_default_align_center')
-        self.enable_view('website.template_header_hamburger_align_center')
         self.enable_view('website.no_autohide_menu')
 
         self.disable_view('website.footer_custom')
