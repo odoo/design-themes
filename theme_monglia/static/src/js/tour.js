fb988850beb4c3a5517cd4aab769d8b9ba785992
xlu-odoo <xlu@odoo.com>
2021-08-11 16:58:51 +0200

    [IMP] theme_monglia: update the theme
    This commit updates the theme to improve the design with new colors, new
    pictures and new snippet customizations.
    
    task-2619534
    
    X-original-commit: a27996e8096f6b727e581b0d8bb21fa9447bab74

--- theme_monglia/static/src/js/tour.js
+++ theme_monglia/static/src/js/tour.js
@@ -1,11 +1,6 @@
-odoo.define("theme_monglia.tour.monglia", function (require) {
-"use strict";
+/** @odoo-module */
 
-const core = require("web.core");
-const _t = core._t;
-
-const wTourUtils = require("website.tour_utils");
-var tour = require("web_tour.tour");
+import wTourUtils from 'website.tour_utils';
 
 const snippets = [
     {
@@ -17,44 +12,40 @@ const snippets = [
         name: 'Title',
     },
     {
+        id: 's_text_block',
+        name: 'Text',
+    },
+    {
+        id: 's_three_columns',
+        name: 'Columns',
+    },
+    {
+        id: 's_image_wall',
+        name: 'Images Wall',
+    },
+    {
+        id: 's_title',
+        name: 'Title',
+    },
+    {
         id: 's_media_list',
         name: 'Media List',
-    },
-    {
-        id: 's_carousel',
-        name: 'Carousel',
-    },
-    {
-        id: 's_call_to_action',
-        name: 'Call to Action',
     },
     {
         id: 's_text_image',
         name: 'Text - Image',
     },
-    {
-        id: 's_image_text',
-        name: 'Image - Text',
-    },
-
 ];
 
 wTourUtils.registerThemeHomepageTour("monglia_tour", [
     wTourUtils.dragNDrop(snippets[0]),
     wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
-
     wTourUtils.dragNDrop(snippets[1]),
     wTourUtils.dragNDrop(snippets[2]),
     wTourUtils.dragNDrop(snippets[3]),
-
     wTourUtils.dragNDrop(snippets[4]),
-    wTourUtils.clickOnSnippet(snippets[4], 'top'),
-    wTourUtils.changeBackgroundColor('left'),
-    wTourUtils.selectColorPalette(),
-    wTourUtils.goBackToBlocks('left'),
-
     wTourUtils.dragNDrop(snippets[5]),
     wTourUtils.dragNDrop(snippets[6]),
+    wTourUtils.dragNDrop(snippets[7]),
 ]);
-});
