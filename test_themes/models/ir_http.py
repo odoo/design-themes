168e013b22825870b68bd31df71c1587e2dd2298
Julien Castiaux <juc@odoo.com>
2021-09-06 10:23:49 +0000

    [REF] core: HTTPocalypse
    Comprehensive refactor of the HTTP framework. Discussions, rationnals
    and changes are found on GitHub.
    
    closes odoo/design-themes#524
    
    Pr: odoo/odoo#78857
    Task: 2571224
    Related: odoo/enterprise#21849
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- test_themes/models/ir_http.py
+++ test_themes/models/ir_http.py
@@ -9,10 +9,10 @@ class Http(models.AbstractModel):
     _inherit = 'ir.http'
 
     @classmethod
-    def _add_dispatch_parameters(cls, func):
+    def _frontend_pre_dispatch(cls):
         # Allow public user to use `fw` query string in test mode to ease tests
         force_website_id = request.httprequest.args.get('fw')
         if (request.registry.in_test_mode() or tools.config.options['test_enable']) and force_website_id:
             request.env['website']._force_website(force_website_id)
 
-        super(Http, cls)._add_dispatch_parameters(func)
+        super(Http, cls)._frontend_pre_dispatch()
