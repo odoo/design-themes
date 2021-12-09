558ca324a59ba31780ed99297bf5da1f001d3550
Benjamin Vray <bvr@odoo.com>
2021-12-09 16:00:40 +0100

    [FIX] theme_*: fix the activation and deactivation of assets
    Before this commit:
    
    - The assets were not enabled or disabled when installing themes because
    we searched for assets with the "key" field instead of the "name" field.
    
    - The "Ripple Effect" was not correctly installed on several themes.
    Because the variable and the assets were not both activated. Even if the
    variable is not used, it is still necessary for the proper functioning
    of the option (e.g. _computeWidgetState).
    
    task-2686370
    
    X-original-commit: 77b2b97f3dce3f0c926eddcd9823e0e86ef66480

--- theme_aviato/models/theme_aviato.py
+++ theme_aviato/models/theme_aviato.py
@@ -8,3 +8,6 @@ class ThemeAviato(models.AbstractModel):
         self.disable_view('website.footer_custom')
         self.enable_view('website.template_footer_contact')
         self.enable_view('website.template_footer_slideout')
+
+        self.enable_asset("website.ripple_effect_scss")
+        self.enable_asset("website.ripple_effect_js")
