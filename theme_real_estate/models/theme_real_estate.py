9a289ea5749e4153e3f2da3af1c7b0f7bb5f873a
Romain Derie <rde@odoo.com>
2021-12-09 18:05:34 +0000

    [FIX] theme_*: enable footer/header template correctly
    See [1], `enable_view()` is now in charge of disabling the already possibly
    existing template header (and/or footer).
    Disabling `website.template_header_default` was not enough, any of the other
    template could have been selected by the user before updating the theme.
    
    Note that when installing a theme the first time on a website, the error won't
    occur as `_reset_default_config()` will be called through `_theme_remove()`.
    
    [1]: https://github.com/odoo/odoo/pull/81181
    
    Fixes https://github.com/odoo/upgrade/pull/3048
    task-2593407
    opw-2680866
    opw-2685951
    opw-2685124
    opw-2679040
    
    closes odoo/design-themes#536
    
    Related: odoo/odoo#81181
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- theme_real_estate/models/theme_real_estate.py
+++ theme_real_estate/models/theme_real_estate.py
@@ -5,8 +5,5 @@ class ThemeRealEstate(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_real_estate_post_copy(self, mod):
-        # For compatibility
-        # self.enable_asset('theme_common.compatibility_saas_10_2')
-
         self.enable_asset('Ripple effect SCSS')
         self.enable_asset('Ripple effect JS')
