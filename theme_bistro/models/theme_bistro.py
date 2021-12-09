429adadb0139bade972cd2879df35f6519529e93
Romain Derie <rde@odoo.com>
2021-12-09 18:05:34 +0000

    [FIX] theme_*: enable footer/header template correctly
    See [1], `enable_view()` is now in charge of disabling the already possibly
    existing template header (and/or footer).
    Disabling `website.template_header_default` was not enough, any of the other
    template could have been selected by the user before updating the theme.
    
    Note that when installing a theme the first time on a website, the error won't
    occur as `_reset_default_config()` will be called through `_theme_remove()`.
    
    [1]: https://github.com/odoo/odoo/pull/81181
    
    Fixes https://github.com/odoo/upgrade/pull/3048
    task-2593407
    opw-2680866
    opw-2685951
    opw-2685124
    opw-2679040
    
    closes odoo/design-themes#547
    
    X-original-commit: 9a289ea5749e4153e3f2da3af1c7b0f7bb5f873a
    Related: odoo/odoo#81952
    Signed-off-by: Romain Derie (rde) <rde@odoo.com>

--- theme_bistro/models/theme_bistro.py
+++ theme_bistro/models/theme_bistro.py
@@ -5,14 +5,12 @@ class ThemeBistro(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_bistro_post_copy(self, mod):
-        self.disable_view('website.template_header_default')
         self.enable_view('website.template_header_vertical')
         self.enable_view('website.template_header_default_align_right')
         self.enable_view('website.template_header_hamburger_align_right')
         self.enable_view('website.header_navbar_pills_style')
         self.enable_header_off_canvas()
 
-        self.disable_view('website.footer_custom')
         self.enable_view('website.template_footer_centered')
         self.enable_view('website.template_footer_slideout')
         self.enable_view('website.option_footer_scrolltop')
--- theme_cobalt/models/theme_cobalt.py
+++ theme_cobalt/models/theme_cobalt.py
@@ -5,9 +5,6 @@ class ThemeCobalt(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_cobalt_post_copy(self, mod):
-        # For compatibility
-        # self.enable_asset('theme_common.compatibility_saas_10_2')
-
         self.enable_asset('Ripple effect SCSS')
         self.enable_asset('Ripple effect JS')
         self.enable_header_off_canvas()
