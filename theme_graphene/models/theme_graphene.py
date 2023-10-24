060c0421c77cb093ea88b4e789d33602e63bdfb8
qsm-odoo <qsm@odoo.com>
2023-10-24 21:27:36 +0200

    [IMP] *: replace `contact` by `sales_three` header
    See community commit for details.
    
    task-3060986
    
    Part-of: odoo/design-themes#651

--- theme_graphene/models/theme_graphene.py
+++ theme_graphene/models/theme_graphene.py
@@ -5,7 +5,7 @@ class ThemeGraphene(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_graphene_post_copy(self, mod):
-        self.enable_view('website.template_header_contact')
+        self.enable_view('website.template_header_sales_two')
 
         self.enable_view('website.template_footer_centered')
 
