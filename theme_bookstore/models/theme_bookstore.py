afeee8aa22341d2b63509b41968aa97f02937aeb
qsm-odoo <qsm@odoo.com>
2023-10-24 21:27:36 +0200

    [IMP] *: replace `contact` by `sales_three` header
    See community commit for details.
    
    task-3060986

--- theme_bookstore/models/theme_bookstore.py
+++ theme_bookstore/models/theme_bookstore.py
@@ -5,6 +5,5 @@ class ThemeBookstore(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_bookstore_post_copy(self, mod):
-        self.enable_view('website.template_header_contact')
+        self.enable_view('website.template_header_sales_two')
         self.enable_view('website.template_footer_links')
-
