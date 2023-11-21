36207ae2290b2581587748364500867d10f1a6f3
Arthur Detroux (ard) <ard@odoo.com>
2023-11-21 10:24:16 +0100

    [FIX] theme_aviator, theme_odoo_experts: Update homepage tours
    Since [1], some blocks in theme_aviato and theme_odoo_experts no longer
    have shapes by default, so add a step to toggle a shape.
    
    [1]: https://github.com/odoo/design-themes/commit/d206c119720d557c11320ebb3d7339890b8f9efa
    
    opw-3595512

--- theme_odoo_experts/static/src/js/tour.js
+++ theme_odoo_experts/static/src/js/tour.js
@@ -45,7 +45,7 @@ wTourUtils.registerThemeHomepageTour("odoo_experts_tour", () => [
     wTourUtils.dragNDrop(snippets[4]),
     wTourUtils.dragNDrop(snippets[5]),
     wTourUtils.clickOnSnippet(snippets[5], 'top'),
-    wTourUtils.changeOption('BackgroundShape', 'we-toggler', _t('Background Shape')),
+    wTourUtils.changeOption('ColoredLevelBackground', 'we-button[data-toggle-bg-shape]', _t('Background Shape')),
     wTourUtils.selectNested('we-select-page', 'BackgroundShape', ':not(.o_we_pager_controls)', _t('Background Shape')),
     wTourUtils.goBackToBlocks(),
     wTourUtils.dragNDrop(snippets[6]),
