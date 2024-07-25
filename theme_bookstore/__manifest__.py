d685208513edd861dfd74a3f553c69f2f3c5b8ba
Antoine (anso) <anso@odoo.com>
2024-07-25 13:30:01 +0200

    [IMP] theme_*: adapt themes with new `s_image_wall` design
    *: (artists, bookstore, cobalt, enark, graphene, monglia, real_estate,
    vehicle, yes, zap)
    
    This commit adapts the design of `s_image_wall` for multiple themes,
    based on the new Odoo 18 snippet redesign.
    
    task-3654301

--- theme_bookstore/__manifest__.py
+++ theme_bookstore/__manifest__.py
@@ -25,6 +25,7 @@
         'views/snippets/s_media_list.xml',
         'views/snippets/s_comparisons.xml',
         'views/snippets/s_features_grid.xml',
+        'views/snippets/s_image_gallery.xml',
         'views/snippets/s_product_catalog.xml',
         'views/snippets/s_quotes_carousel.xml',
         'views/new_page_template.xml',
