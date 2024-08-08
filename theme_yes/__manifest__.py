aca3f875203da0262660688c668fa50f35e36948
Chrysanthe (chgo) <chgo@odoo.com>
2024-08-08 13:14:07 +0200

    [IMP] theme_* : adapt `s_kickoff` images within themes
    *: anelusia, artists, aviato, beauty, bewise, bisro, bookstore, buzzy,
    clean, cobalt, graphene, kea, kiddo loftspace, monglia, nano, notes,
    odoo_experts, orchid, paptic, treehouse, vehicle, yes, zap.
    
    This commit reviews the occurrences of the `s_kickoff` snippet across
    design themes.
    
    task-4077609
    Part of task-4077427

--- theme_yes/__manifest__.py
+++ theme_yes/__manifest__.py
@@ -38,6 +38,7 @@
         'views/snippets/s_image_hexagonal.xml',
         'views/snippets/s_striped_center_top.xml',
         'views/snippets/s_key_images.xml',
+        'views/snippets/s_kickoff.xml',
         'views/new_page_template.xml',
     ],
     'images': [
