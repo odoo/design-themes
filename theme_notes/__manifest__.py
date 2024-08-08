f2b03ac7bf40ff46d5da4b5ff6f4a74085310477
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-08-08 17:33:55 +0200

    [IMP] theme_*: adapt themes with new `s_unveil` snippet
    *: anelusia, avantgarde, aviato, beauty, bewise, bookstore, ernark,
    graphene, kea, kiddo, loftspace, monglia, nano, notes, odoo experts,
    orchid, paptic, real_estate, treehouse, vehicle, yes, zap
    
    This commit adapts the design of new s_unveil snippet for multiple
    themes.
    
    task-4104471
    
    closes odoo/design-themes#860
    
    Part-of: task-4077427
    Related: odoo/odoo#176167
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- theme_notes/__manifest__.py
+++ theme_notes/__manifest__.py
@@ -30,6 +30,7 @@
         'views/snippets/s_parallax.xml',
         'views/snippets/s_comparisons.xml',
         'views/snippets/s_quotes_carousel.xml',
+        'views/snippets/s_unveil.xml',
         'views/new_page_template.xml',
     ],
     'images': [
