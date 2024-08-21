9ed1f2eeb3d0bb18875c26e162232462906e5c07
Chrysanthe (chgo) <chgo@odoo.com>
2024-08-21 18:25:38 +0200

    [IMP] theme_*: review s_images_constellation design across themes
    *: anelusia, artists, avantgarde, aviato, beauty, bewise, bistro,
    bookstore, buzzy, clean, cobalt, enark, graphene, kea, kiddo, loftspace,
    monglia, nano, notes, odoo_experts, orchid, paptic, real_estate,
    treehouse, vehicle, yes, zap
    
    This commit reviews the occurrences of the `s_images_constellation`
    snippet across design themes.
    
    task-4105366
    part of task-4077427

--- theme_clean/__manifest__.py
+++ theme_clean/__manifest__.py
@@ -34,6 +34,7 @@
         'views/snippets/s_striped_center_top.xml',
         'views/snippets/s_big_number.xml',
         'views/snippets/s_image_frame.xml',
+        'views/snippets/s_images_constellation.xml',
         'views/new_page_template.xml',
     ],
     'images': [
--- theme_enark/__manifest__.py
+++ theme_enark/__manifest__.py
@@ -37,6 +37,7 @@
         'views/snippets/s_intro_pill.xml',
         'views/snippets/s_big_number.xml',
         'views/snippets/s_image_frame.xml',
+        'views/snippets/s_images_constellation.xml',
         'views/new_page_template.xml',
     ],
     'images': [
