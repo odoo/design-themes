7e16187114fc538b44d4a0c5ff7cc6334f443ef7
mano-odoo <mano@odoo.com>
2024-08-30 17:40:31 +0200

    [IMP] theme_*: adapt `s_company_team_basic`
    task-4144648
    Part of: task-4077427
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- theme_yes/__manifest__.py
+++ theme_yes/__manifest__.py
@@ -19,6 +19,7 @@
         'views/snippets/s_carousel.xml',
         'views/snippets/s_company_team.xml',
         'views/snippets/s_company_team_detail.xml',
+        'views/snippets/s_company_team_basic.xml',
         'views/snippets/s_cover.xml',
         'views/snippets/s_card_offset.xml',
         'views/snippets/s_image_text.xml',
