6c1b82ab0c97d321dd58a4360f5851acb0f5de3a
Chrysanthe (chgo) <chgo@odoo.com>
2024-09-13 17:55:14 +0000

    [IMP] theme_loftspace: revamp the theme
    This commit revamp the design of the `theme_loftspace`.
    
    task-4178079
    part of task-4177975
    
    X-original-commit: fa220d01700221261abda86fbe9921bd5916aaac

--- theme_loftspace/static/src/js/tour.js
+++ theme_loftspace/static/src/js/tour.js
@@ -4,28 +4,43 @@ import * as wTourUtils from '@website/js/tours/tour_utils';
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
+        id: 's_banner',
+        name: 'Banner',
         groupName: "Intro",
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
-        groupName: "Columns",
-    },
-    {
-        id: 's_title',
-        name: 'Title',
+        id: 's_text_block',
+        name: 'Text',
         groupName: "Text",
     },
     {
-        id: 's_images_wall',
-        name: 'Images Wall',
-        groupName: "Images",
+        id: 's_striped',
+        name: 'Striped section',
+        groupName: "Content",
     },
     {
-        id: 's_call_to_action',
-        name: 'Call to Action',
+        id: 's_key_images',
+        name: 'Key Images',
+        groupName: "Columns",
+    },
+    {
+        id: 's_features',
+        name: 'Features',
+        groupName: "Content",
+    },
+    {
+        id: 's_quotes_carousel',
+        name: 'Quotes',
+        groupName: "People",
+    },
+    {
+        id: 's_faq_collapse',
+        name: 'FAQ Block',
+        groupName: "Text",
+    },
+    {
+        id: 's_cta_box',
+        name: 'Box Call to Action',
         groupName: "Content",
     },
 ];
@@ -39,6 +54,10 @@ wTourUtils.registerThemeHomepageTour("loftspace_tour", () => [
     ...wTourUtils.dragNDrop(snippets[2]),
     ...wTourUtils.dragNDrop(snippets[3]),
     ...wTourUtils.dragNDrop(snippets[4]),
+    ...wTourUtils.dragNDrop(snippets[5]),
+    ...wTourUtils.dragNDrop(snippets[6]),
+    ...wTourUtils.dragNDrop(snippets[7]),
+    ...wTourUtils.dragNDrop(snippets[8]),
     ...wTourUtils.clickOnSnippet(snippets[4]),
     wTourUtils.changeBackgroundColor(),
     wTourUtils.selectColorPalette(),
