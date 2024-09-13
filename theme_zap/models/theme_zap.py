8e1fd68ebbca11e274404867d6050783d7622a78
Antoine (anso) <anso@odoo.com>
2024-09-13 12:45:07 +0000

    [IMP] theme_zap: theme improvement
    Revamp of `theme_zap`
    
    task-4178094
    
    closes odoo/design-themes#950
    
    X-original-commit: 0dd9e17b8f27fc22e9c22a38b9ec6e2fc352c592
    Signed-off-by: Alice Gaudon (agau) <agau@odoo.com>
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_zap/models/theme_zap.py
+++ theme_zap/models/theme_zap.py
@@ -5,9 +5,7 @@ class ThemeZap(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_zap_post_copy(self, mod):
-        self.enable_view('website.template_header_hamburger')
-        self.enable_view('website.no_autohide_menu')
-        self.enable_view('website.header_navbar_pills_style')
+        self.enable_view('website.template_header_sales_four')
 
         self.enable_view('website.template_footer_links')
 
