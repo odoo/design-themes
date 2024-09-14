917f07d21c3c40c9cbfdc862f1a03482d1cfa60b
Chrysanthe (chgo) <chgo@odoo.com>
2024-09-14 22:57:45 +0200

    [IMP] theme_odoo_experts: revamp the theme
    task-4178086
    part of task-4177975
    
    X-original-commit: 238f347935626c68c719ccfc56b2acace79635ef

--- theme_odoo_experts/models/theme_odoo_experts.py
+++ theme_odoo_experts/models/theme_odoo_experts.py
@@ -5,5 +5,4 @@ class ThemeOdooExperts(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_odoo_experts_post_copy(self, mod):
-        self.enable_view('website.template_header_sales_three')
         self.enable_view('website.template_footer_contact')
