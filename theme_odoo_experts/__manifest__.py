54fc022773e86768ac3df4ffcf98e773a5f4f105
Chrysanthe (chgo) <chgo@odoo.com>
2024-09-14 22:57:45 +0200

    [IMP] theme_odoo_experts: revamp the theme
    task-4178086
    part of task-4177975
    
    X-original-commit: 238f347935626c68c719ccfc56b2acace79635ef
    Part-of: odoo/design-themes#971
    Related: odoo/odoo#182039
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_odoo_experts/__manifest__.py
+++ theme_odoo_experts/__manifest__.py
@@ -49,6 +49,9 @@
         'views/snippets/s_big_number.xml',
         'views/snippets/s_wavy_grid.xml',
         'views/snippets/s_empowerment.xml',
+        'views/snippets/s_numbers_list.xml',
+        'views/snippets/s_showcase.xml',
+        'views/snippets/s_mockup_image.xml',
         'views/new_page_template.xml',
     ],
     'images': [
@@ -56,11 +59,15 @@
         'static/description/odoo_experts_screenshot.jpg',
     ],
     'images_preview_theme': {
-        'website.s_picture_default_image': '/theme_odoo_experts/static/src/img/snippets/s_picture.jpg',
+        'website.s_mockup_image_default_image': '/theme_odoo_experts/static/src/img/snippets/s_mockup_image_default_image.jpg',
         'website.s_text_image_default_image': '/theme_odoo_experts/static/src/img/snippets/s_text_image.jpg',
+        'website.s_image_text_default_image': '/theme_odoo_experts/static/src/img/snippets/s_image_text.jpg',
+        'website.s_showcase_default_image': '/theme_odoo_experts/static/src/img/snippets/s_showcase_default_image.jpg',
+        'website.library_image_08': '/theme_odoo_experts/static/src/img/snippets/library_image_08.jpg',
+        'website.s_picture_default_image': '/theme_odoo_experts/static/src/img/snippets/s_picture.jpg',
     },
     'configurator_snippets': {
-        'homepage': ['s_picture', 's_references', 's_image_text', 's_text_image', 's_title', 's_comparisons', 's_call_to_action'],
+        'homepage': ['s_mockup_image', 's_references', 's_image_text', 's_text_image', 's_showcase', 's_faq_collapse', 's_cta_box'],
     },
     'new_page_templates': {
         'about': {
