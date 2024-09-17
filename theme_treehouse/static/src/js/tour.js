64e311d0c8178599c7e387661989b00a3aa86d14
Brieuc-brd <brd@odoo.com>
2024-09-17 15:44:02 +0200

    [IMP] theme_treehouse: revamp the theme
    task-4178089
    Part of task-4177975
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- theme_treehouse/static/src/js/tour.js
+++ theme_treehouse/static/src/js/tour.js
@@ -4,34 +4,34 @@ import * as wTourUtils from '@website/js/tours/tour_utils';
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
+        id: 's_sidegrid',
+        name: 'Side grid',
         groupName: "Intro",
     },
     {
-        id: 's_text_image',
-        name: 'Text - Image',
+        id: 's_numbers_list',
+        name: 'Numbers list',
         groupName: "Content",
     },
     {
-        id: 's_title',
-        name: 'Title',
-        groupName: "Text",
+        id: 's_color_blocks_2',
+        name: 'Big Boxes',
+        groupName: "Content",
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
+        id: 's_references',
+        name: 'References',
+        groupName: "People",
+    },
+    {
+        id: 's_freegrid',
+        name: 'Free grid',
         groupName: "Columns",
-    },
-    {
-        id: 's_call_to_action',
-        name: 'Call to Action',
-        groupName: "Content",
     },
 ];
 
 wTourUtils.registerThemeHomepageTour("treehouse_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"treehouse-1"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-28"'),
     ...wTourUtils.dragNDrop(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
