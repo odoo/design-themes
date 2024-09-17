43423ccdd9e39e806341faa95a3f9e125f0a3fa0
Brieuc-brd <brd@odoo.com>
2024-09-17 15:44:02 +0200

    [IMP] theme_treehouse: revamp the theme
    task-4178089
    Part of task-4177975

--- theme_treehouse/static/src/js/tour.js
+++ theme_treehouse/static/src/js/tour.js
@@ -4,29 +4,29 @@ import * as wTourUtils from '@website/js/tours/tour_utils';
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
+        id: 's_sidegrid',
+        name: 'Side grid',
         groupName: "Intro",
     },
     {
-        id: 's_text_image',
-        name: 'Text - Image',
+        id: 's_numbers_list',
+        name: 'Numbers list',
         groupName: "Content",
     },
     {
-        id: 's_title',
-        name: 'Title',
-        groupName: "Text",
+        id: 's_color_blocks_2',
+        name: 'Big Boxes',
+        groupName: "Content",
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
+        id: 's_references',
+        name: 'References',
+        groupName: "People",
+    },
+    {
+        id: 's_freegrid',
+        name: 'Free grid',
         groupName: "Columns",
-    },
-    {
-        id: 's_call_to_action',
-        name: 'Call to Action',
-        groupName: "Content",
     },
 ];
 
