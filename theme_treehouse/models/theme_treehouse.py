fa78b350cc41315bee4f43f8096cafae56e9c5e1
Brieuc-brd <brd@odoo.com>
2024-09-17 15:44:02 +0200

    [IMP] theme_treehouse: revamp the theme
    task-4178089
    Part of task-4177975
    
    Part-of: odoo/design-themes#919
    Related: odoo/odoo#180736
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_treehouse/models/theme_treehouse.py
+++ theme_treehouse/models/theme_treehouse.py
@@ -7,10 +7,10 @@ class ThemeTreehouse(models.AbstractModel):
     def _theme_treehouse_post_copy(self, mod):
         self.disable_view('website.header_visibility_standard')
         self.enable_view('website.header_visibility_fixed')
+        self.enable_view('website.template_header_vertical')
 
-        self.enable_view('website.template_footer_contact')
+        self.enable_view('website.template_footer_descriptive')
         self.enable_view('website.template_footer_slideout')
-        self.enable_view('website.option_footer_scrolltop')
 
         self.enable_asset("website.ripple_effect_scss")
         self.enable_asset("website.ripple_effect_js")
