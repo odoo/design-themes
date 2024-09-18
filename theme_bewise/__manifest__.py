226453f6f902093d931fa24f9d7252dc4be0afd1
stefanorigano (SRI) <sri@odoo.com>
2024-09-18 19:54:16 +0200

    [IMP] theme_bewise: revamp the theme
    task-4178020
    part of task-4177975
    
    Signed-off-by: Outagant Mehdi (mou) <mou@odoo.com>

--- theme_bewise/__manifest__.py
+++ theme_bewise/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Theme/Education',
     'summary': 'University, Education, Schools, Young, Play, Kids',
     'sequence': 240,
-    'version': '2.1.0',
+    'version': '3.0.0',
     'depends': ['theme_common'],
     'data': [
         'data/generate_primary_template.xml',
@@ -18,15 +18,13 @@
         'static/description/bewise_screenshot.jpg',
     ],
     'images_preview_theme': {
-        'website.s_cover_default_image': '/theme_bewise/static/src/img/backgrounds/college_bg.jpg',
-        'website.s_text_image_default_image': '/theme_bewise/static/src/img/content/college_graduate.jpg',
-        'website.s_image_text_default_image': '/theme_bewise/static/src/img/content/college_students.jpg',
+        'website.s_picture_default_image': '/theme_bewise/static/src/img/content/college_library.jpg',
         'website.s_media_list_default_image_1': '/theme_bewise/static/src/img/content/college_media_1.jpg',
         'website.s_media_list_default_image_2': '/theme_bewise/static/src/img/content/college_media_2.jpg',
-        'website.s_quotes_carousel_demo_image_0': '/theme_bewise/static/src/img/backgrounds/college_bg_4.jpg',
+        'website.s_masonry_block_default_image_1': '/theme_bewise/static/src/img/content/content_img_25.jpg',
     },
     'configurator_snippets': {
-        'homepage': ['s_cover', 's_call_to_action', 's_text_image', 's_numbers', 's_image_text', 's_quotes_carousel', 's_color_blocks_2'],
+        'homepage': ['s_striped_center_top', 's_title', 's_color_blocks_2', 's_faq_collapse', 's_masonry_block', 's_company_team_shapes'],
     },
     'license': 'LGPL-3',
     'live_test_url': 'https://theme-bewise.odoo.com',
