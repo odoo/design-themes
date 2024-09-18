dddd841c9bdd54848885bc6f0b93405c4f77c8f4
Benjamin Vray <bvr@odoo.com>
2024-09-18 11:16:27 +0200

    [FIX] theme_*: rename the dragNDrop function to insertSnippet
    This commit replaces the "dragNDrop" function used in theme tours with
    "insertSnippet" because the function name has changed. Now, users no
    longer need to drag and drop categories onto the page from the snippet
    menu. Instead, they now need to click on them.
    
    task-4072655
    
    closes odoo/design-themes#916
    
    Related: odoo/odoo#180471
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_monglia/static/src/js/tour.js
+++ theme_monglia/static/src/js/tour.js
@@ -47,14 +47,14 @@ const snippets = [
 
 wTourUtils.registerThemeHomepageTour("monglia_tour", () => [
     wTourUtils.assertCssVariable('--color-palettes-name', '"monglia-1"'),
-    ...wTourUtils.dragNDrop(snippets[0]),
+    ...wTourUtils.insertSnippet(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
-    ...wTourUtils.dragNDrop(snippets[1]),
-    ...wTourUtils.dragNDrop(snippets[2]),
-    ...wTourUtils.dragNDrop(snippets[3]),
-    ...wTourUtils.dragNDrop(snippets[4]),
-    ...wTourUtils.dragNDrop(snippets[5]),
-    ...wTourUtils.dragNDrop(snippets[6]),
-    ...wTourUtils.dragNDrop(snippets[7]),
+    ...wTourUtils.insertSnippet(snippets[1]),
+    ...wTourUtils.insertSnippet(snippets[2]),
+    ...wTourUtils.insertSnippet(snippets[3]),
+    ...wTourUtils.insertSnippet(snippets[4]),
+    ...wTourUtils.insertSnippet(snippets[5]),
+    ...wTourUtils.insertSnippet(snippets[6]),
+    ...wTourUtils.insertSnippet(snippets[7]),
 ]);
