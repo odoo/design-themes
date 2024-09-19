18d8c6071885b31fdc07a877a3d472fc95c1d8e3
Chrysanthe (chgo) <chgo@odoo.com>
2024-09-19 14:18:34 +0000

    [IMP] theme_yes: review theme design
    task-4178092
    part of task-4177975
    
    X-original-commit: 97635844950ae6a040f2af26852ebbefc4c10605

--- theme_yes/static/src/js/tour.js
+++ theme_yes/static/src/js/tour.js
@@ -4,8 +4,8 @@ import * as wTourUtils from '@website/js/tours/tour_utils';
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
+        id: 's_kickoff',
+        name: 'Kickoff',
         groupName: "Intro",
     },
     {
@@ -19,19 +19,19 @@ const snippets = [
         groupName: "People",
     },
     {
-        id: 's_media_list',
-        name: 'Media List',
+        id: 's_image_text_overlap',
+        name: 'Image - Text Overlap',
         groupName: "Content",
     },
     {
-        id: 's_images_wall',
-        name: 'Images Wall',
-        groupName: "Images",
+        id: 's_features',
+        name: 'Features',
+        groupName: "Content>",
     },
     {
-        id: 's_quotes_carousel',
-        name: 'Quotes',
-        groupName: "People",
+        id: 's_call_to_action',
+        name: 'Call to Action',
+        groupName: "Content",
     },
 ];
 
