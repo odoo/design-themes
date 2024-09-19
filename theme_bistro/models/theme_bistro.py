97683ee28357e656172522d9e62dcfbed5fdb4cd
Brieuc-brd <brd@odoo.com>
2024-09-19 10:58:09 +0200

    [IMP] theme_bistro: revamp the theme
    task-4178021
    Part of task-4177975
    
    closes odoo/design-themes#919
    
    Related: odoo/odoo#180736
    Signed-off-by: Outagant Mehdi (mou) <mou@odoo.com>
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_bistro/models/theme_bistro.py
+++ theme_bistro/models/theme_bistro.py
@@ -8,9 +8,5 @@ class ThemeBistro(models.AbstractModel):
         self.enable_view('website.template_header_vertical')
         self.enable_view('website.header_navbar_pills_style')
 
-        self.enable_view('website.template_footer_centered')
-        self.enable_view('website.template_footer_slideout')
-        self.enable_view('website.option_footer_scrolltop')
-
         self.enable_asset("website.ripple_effect_scss")
         self.enable_asset("website.ripple_effect_js")
