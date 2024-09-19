9cf70ae8e9461caac9e83e4fe411f4ed2789f62e
Brieuc-brd <brd@odoo.com>
2024-09-19 16:46:31 +0200

    [IMP] theme_beauty: revamp the theme
    task-4178019
    Part of task-4177975
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- theme_beauty/models/theme_beauty.py
+++ theme_beauty/models/theme_beauty.py
@@ -5,5 +5,4 @@ class ThemeBeauty(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_beauty_post_copy(self, mod):
-        self.enable_view('website.template_header_sales_one')
         self.enable_view('website.footer_custom')
