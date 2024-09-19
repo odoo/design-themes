0bb5f6c93e1fc7fd43fb935d4ff6eb993374ec4f
mano-odoo <mano@odoo.com>
2024-09-19 18:09:29 +0200

    [IMP] theme_notes: revamp theme
    Revamp the theme design and adapt its existing snippets
    
    task-4178083
    Part of task-4177975
    
    closes odoo/design-themes#927
    
    Signed-off-by: Alice Gaudon (agau) <agau@odoo.com>

--- theme_notes/static/src/js/tour.js
+++ theme_notes/static/src/js/tour.js
@@ -5,39 +5,49 @@ import { _t } from "@web/core/l10n/translation";
 
 const snippets = [
     {
-        id: 's_carousel',
-        name: 'Carousel',
+        id: 's_framed_intro',
+        name: 'Framed Intro',
         groupName: "Intro",
     },
     {
-        id: 's_masonry_block',
-        name: 'Masonry',
+        id: 's_image_text',
+        name: 'Image Text',
         groupName: "Images",
     },
     {
+        id: 's_three_columns',
+        name: 'Three Columns',
+        groupName: "Content",
+    },
+    {
+        id: 's_images_wall',
+        name: 'Images Wall',
+        groupName: "Content",
+    },
+    {
         id: 's_text_image',
-        name: 'Text - Image',
+        name: 'Text Image',
         groupName: "Content",
     },
     {
-        id: 's_product_catalog',
-        name: 'Pricelist',
-        groupName: "Content",
-    },
-    {
-        id: 's_media_list',
-        name: 'Media List',
-        groupName: "Content",
-    },
-    {
-        id: 's_company_team',
+        id: 's_company_team_shapes',
         name: 'Team',
         groupName: "People",
+    },
+    {
+        id: 's_title',
+        name: 'Title',
+        groupName: "Content",
+    },
+    {
+        id: 's_call_to_action',
+        name: 'Call to Action',
+        groupName: "Content",
     },
 ];
 
 wTourUtils.registerThemeHomepageTour("notes_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"notes-1"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-19"'),
     ...wTourUtils.dragNDrop(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h2'),
     wTourUtils.goBackToBlocks(),
@@ -49,4 +59,6 @@ wTourUtils.registerThemeHomepageTour("notes_tour", () => [
     ...wTourUtils.dragNDrop(snippets[3]),
     ...wTourUtils.dragNDrop(snippets[4]),
     ...wTourUtils.dragNDrop(snippets[5]),
+    ...wTourUtils.dragNDrop(snippets[6]),
+    ...wTourUtils.dragNDrop(snippets[7]),
 ]);
