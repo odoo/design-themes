23b2dd1971192461dc29ee4c7b2968343dc5dfe2
mano-odoo <mano@odoo.com>
2024-09-23 15:11:57 +0200

    [IMP] theme_artists: revamp the theme
    Revamp the themes, add new images and adapt the existing snippets to fit
    the new design.
    
    task-4178014
    Part of task-4177975
    
    X-original-commit: 14993a5a1e54d7d25c793209637e0efd8cb286c7
    Part-of: odoo/design-themes#971
    Related: odoo/odoo#182039
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_artists/models/theme_artists.py
+++ theme_artists/models/theme_artists.py
@@ -5,7 +5,7 @@ class ThemeArtists(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_artists_post_copy(self, mod):
-        self.enable_view('website.template_header_hamburger')
+        self.enable_view('website.template_header_vertical')
         self.enable_view('website.no_autohide_menu')
 
         self.enable_view('website.template_footer_centered')
