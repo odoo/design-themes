2b8f38818a3f3a80b346f81c3c9e68f4d3aca0f6
Brieuc-brd <brd@odoo.com>
2024-09-23 17:09:02 +0200

    [IMP] theme_cobalt: revamp the theme
    task-4178067
    Part of task-4177978

--- theme_cobalt/models/theme_cobalt.py
+++ theme_cobalt/models/theme_cobalt.py
@@ -7,3 +7,6 @@ class ThemeCobalt(models.AbstractModel):
     def _theme_cobalt_post_copy(self, mod):
         self.enable_asset("website.ripple_effect_scss")
         self.enable_asset("website.ripple_effect_js")
+        self.enable_view("website.template_header_boxed")
+        self.enable_view("website.template_footer_call_to_action")
+        self.enable_view("website.header_navbar_pills_style")
--- theme_cobalt/static/src/js/tour.js
+++ theme_cobalt/static/src/js/tour.js
@@ -9,9 +9,14 @@ const snippets = [
         groupName: "Intro",
     },
     {
-        id: 's_references',
-        name: 'References',
-        groupName: "People",
+        id: 's_image_text',
+        name: 'Image - Text',
+        groupName: "Content",
+    },
+    {
+        id: 's_key_images',
+        name: 'Key Images',
+        groupName: "Columns",
     },
     {
         id: 's_text_image',
@@ -19,33 +24,26 @@ const snippets = [
         groupName: "Content",
     },
     {
-        id: 's_color_blocks_2',
-        name: 'Big Boxes',
-        groupName: "Content",
+        id: 's_company_team_detail',
+        name: 'Team Detail',
+        groupName: "People",
     },
     {
-        id: 's_title',
-        name: 'Title',
-        groupName: "Text",
-    },
-    {
-        id: 's_image_gallery',
-        name: 'Images Wall',
-        groupName: "Images",
+        id: 's_references_grid',
+        name: 'References Grid',
+        groupName: "People",
     },
 ];
 
 
 wTourUtils.registerThemeHomepageTour("cobalt_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"cobalt-1"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-8"'),
     ...wTourUtils.dragNDrop(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
-
     ...wTourUtils.dragNDrop(snippets[1]),
     ...wTourUtils.dragNDrop(snippets[2]),
     ...wTourUtils.dragNDrop(snippets[3]),
-
     ...wTourUtils.dragNDrop(snippets[4]),
     ...wTourUtils.dragNDrop(snippets[5]),
 ]);
