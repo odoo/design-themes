caf431c6f9d65e4f2c49627baf15b5cd0756d1a9
Xavier Luyckx (xlu) <xlu@odoo.com>
2024-09-25 10:26:01 +0200

    [IMP] theme_monglia: revamp the theme
    task-4178080
    Part of task-4177975

--- theme_monglia/static/src/js/tour.js
+++ theme_monglia/static/src/js/tour.js
@@ -9,9 +9,14 @@ const snippets = [
         groupName: "Intro",
     },
     {
-        id: 's_title',
-        name: 'Title',
-        groupName: "Text",
+        id: 's_numbers_grid',
+        name: 'Numbers Grid',
+        groupName: "Content",
+    },
+    {
+        id: 's_company_team_shapes',
+        name: 'Team Shapes',
+        groupName: "People",
     },
     {
         id: 's_text_block',
@@ -19,14 +24,19 @@ const snippets = [
         groupName: "Text",
     },
     {
-        id: 's_three_columns',
-        name: 'Columns',
+        id: 's_freegrid',
+        name: 'Free grid',
         groupName: "Columns",
     },
     {
-        id: 's_images_wall',
-        name: 'Images Wall',
-        groupName: "Images",
+        id: 's_cta_box',
+        name: 'Box Call to Action',
+        groupName: "Content",
+    },
+    {
+        id: 's_shape_image',
+        name: 'Shape image',
+        groupName: "Content",
     },
     {
         id: 's_title',
@@ -34,19 +44,24 @@ const snippets = [
         groupName: "Text",
     },
     {
-        id: 's_media_list',
-        name: 'Media List',
-        groupName: "Content",
+        id: 's_images_wall',
+        name: 'Images Wall',
+        groupName: "Images",
     },
     {
-        id: 's_text_image',
-        name: 'Text - Image',
-        groupName: "Content",
+        id: 's_faq_collapse',
+        name: 'FAQ',
+        groupName: "text",
+    },
+    {
+        id: 's_references',
+        name: 'References',
+        groupName: "People",
     },
 ];
 
 wTourUtils.registerThemeHomepageTour("monglia_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"monglia-1"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-light-3"'),
     ...wTourUtils.dragNDrop(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
@@ -57,4 +72,7 @@ wTourUtils.registerThemeHomepageTour("monglia_tour", () => [
     ...wTourUtils.dragNDrop(snippets[5]),
     ...wTourUtils.dragNDrop(snippets[6]),
     ...wTourUtils.dragNDrop(snippets[7]),
+    ...wTourUtils.dragNDrop(snippets[8]),
+    ...wTourUtils.dragNDrop(snippets[9]),
+    ...wTourUtils.dragNDrop(snippets[10]),
 ]);
