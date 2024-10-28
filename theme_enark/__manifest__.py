322223887b2c45fd6d21b363c6682b6feb741a24
Chrysanthe (chgo) <chgo@odoo.com>
2024-10-28 10:47:36 +0100

    [IMP] theme_*: adapt `s_numbers_framed` across design themes
    *: artists, avantgarde, aviato, beauty, bewise, bistro, bookstore, buzzy,
    clean, cobalt, enark, graphene, kea, kiddo, loftspace, monglia, nano,
    botes, odoo_experts, orchid, paptic, real_estate, treehouse, vehicle,
    yes,zap.
    
    This commit adapts the design of the `s_numbers_framed` occurrences
    across design themes.
    
    task-4094384
    Part of task-4077427

--- theme_enark/__manifest__.py
+++ theme_enark/__manifest__.py
@@ -55,6 +55,7 @@
         'views/snippets/s_numbers_boxed.xml',
         'views/snippets/s_company_team_card.xml',
         'views/snippets/s_split_intro.xml',
+        'views/snippets/s_numbers_framed.xml',
         'views/new_page_template.xml',
     ],
     'images': [
