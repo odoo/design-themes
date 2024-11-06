4a178c598c7fd0285f872f9bdfe9eb1152a86258
Chrysanthe (chgo) <chgo@odoo.com>
2024-11-06 15:33:13 +0100

    [IMP] theme_*: adapt `s_form_aside` snippet across themes
    *: artists, nano, notes, vehicle
    
    This commit adapt the occurrences of the `s_form_aside` snippet across
    themes.
    
    task-4135499
    part of task-4077427

--- theme_nano/__manifest__.py
+++ theme_nano/__manifest__.py
@@ -50,6 +50,7 @@
         'views/snippets/s_cta_mobile.xml',
         'views/snippets/s_company_team_spotlight.xml',
         'views/snippets/s_website_form_cover.xml',
+        'views/snippets/s_form_aside.xml',
         'views/new_page_template.xml',
     ],
     'images': [
