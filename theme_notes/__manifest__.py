3e1fe98a4efb6859f1c045f54de1c8bc3a7fa12f
Chrysanthe (chgo) <chgo@odoo.com>
2024-11-12 17:00:43 +0100

    [IMP] theme_*: adapt `s_website_form_cover` snippet
    *: theme_anelusia, theme_artists, theme_avantgarde, theme_aviato,
    theme_beauty, theme_bewise, theme_bistro, theme_bookstore, theme_clean,
    theme_cobalt, theme_enark, theme_graphene, theme_kea, theme_kiddo,
    theme_monglia, theme_nano, theme_notes, theme_odoo_experts, theme_orchid
    , theme_paptic, theme_real_estate, theme_treehouse, theme_vehicle,
    theme_yes, theme_zap
    
    This commit reviews the occurrences of `s_website_form_cover` across
    design themes.
    
    task-4138250
    
    closes odoo/design-themes#1010
    
    Part-of: task-4077427
    Related: odoo/odoo#178102
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_notes/__manifest__.py
+++ theme_notes/__manifest__.py
@@ -64,6 +64,7 @@
         'views/snippets/s_shape_image.xml',
         'views/snippets/s_text_cover.xml',
         'views/snippets/s_empowerment.xml',
+        'views/snippets/s_website_form_cover.xml',
         'views/new_page_template.xml',
     ],
     'images': [
