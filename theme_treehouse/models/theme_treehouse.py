89ef56bda918a77daf2ce6db29e1fcd719cce94d
Gorash <chm@odoo.com>
2024-12-11 13:33:03 +0000

    [IMP] orm/all: Make _name or _inherit mandatory
    This is a request following the revert of the use of class names
    (concerning the python inheritance and typing project)
    
    closes odoo/design-themes#1040
    
    X-original-commit: 2263661435385e4237aafef7a47df563555a7780
    Related: odoo/odoo#191963
    Related: odoo/enterprise#76269
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- theme_treehouse/models/theme_treehouse.py
+++ theme_treehouse/models/theme_treehouse.py
@@ -2,7 +2,7 @@ from odoo import models
 
 
 class ThemeUtils(models.AbstractModel):
-    _inherit = ['theme.utils']
+    _inherit = 'theme.utils'
 
     def _theme_treehouse_post_copy(self, mod):
         self.disable_view('website.header_visibility_standard')
