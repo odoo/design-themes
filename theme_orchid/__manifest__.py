a712081b61912421366c9c5271c2d3e487e8bf6f
Brieuc-brd <brd@odoo.com>
2025-05-07 14:11:36 +0200

    [IMP] theme_artists, *: adapt `s_ecomm_cols_info_h`
    *: theme_loftspace, theme_nano, theme_orchid
    
    This commit adapts customizations for `s_ecomm_cols_info_h`.
    
    task-4775848
    Part of task-4252024

--- theme_orchid/__manifest__.py
+++ theme_orchid/__manifest__.py
@@ -12,6 +12,7 @@
         'views/images.xml',
 
         'views/snippets/s_cta_box.xml',
+        'views/snippets/s_ecomm_cols_info_h.xml',
         'views/snippets/s_cover.xml',
         'views/snippets/s_cards_grid.xml',
         'views/snippets/s_striped_top.xml',
