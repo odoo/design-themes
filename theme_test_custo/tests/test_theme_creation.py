fb696876038b5866bbd6ea3404f5d608d47fb94c
qsm-odoo <qsm@odoo.com>
2025-05-15 12:56:51 +0000

    [FIX] theme_test_custo: fix nightly theme preview tour
    The theme preview feature before selection was disabled in [1]. This
    commit disables the related tour steps that were testing it a nightly
    tour... that was red since then.
    
    [1]: https://github.com/odoo/odoo/commit/7cb71e9479df0ee9af0b7ad39302857666726177
    
    Related to task-3454790
    
    closes odoo/design-themes#1080
    
    X-original-commit: 9580ef86ac60830fb9472ca20839063c3fab7567
    Related: odoo/odoo#210221
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- theme_test_custo/tests/test_theme_creation.py
+++ theme_test_custo/tests/test_theme_creation.py
@@ -3,6 +3,7 @@
 from odoo.tests import HttpCase, tagged
 from odoo.addons.website.tests.test_configurator import TestConfiguratorCommon
 
+
 # TODO: `test_themes` tag should not be there, runbot config should be adapted
 #       to test this module too. There is a special config for the theme repo.
 @tagged('post_install', '-at_install', 'test_themes')
@@ -13,6 +14,7 @@ class Crawler(HttpCase):
         website.theme_id = theme_custo.id
         theme_custo.with_context(load_all_views=True, apply_new_theme=True)._theme_load(website)
         self.start_tour('/@/example', "theme_menu_hierarchies", login='admin')
+
 
 @tagged('post_install', '-at_install')
 class TestThemeConfigurator(TestConfiguratorCommon):
