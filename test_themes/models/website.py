ad596bab4d94b58a120d3ca1cd03da4de98a116d
Serhii Rubanskyi <seru@odoo.com>
2026-02-25 16:33:17 +0000

    [FIX] test_themes: adapt standalone tests to use force_delete
    After commit [1], website standalone tests were failing as they still
    used the old `_force_unlink_`. It actually was replaced by
    `force_delete`, so we should adapt the tests to use it instead.
    
    runbot-237870
    
    [1]: https://github.com/odoo/odoo/commit/4400130eb320cb221f1b7b7720f58099be93b746
    
    X-original-commit: ac8a3d52f5660ad093af5797384df3d844c7e67d

--- test_themes/models/website.py
+++ test_themes/models/website.py
@@ -26,6 +26,6 @@ class Website(models.Model):
         if self in websites_themes:
             # Bypass foreign key constraint
             website_domain = [('website_id', '=', self.id)]
-            self.env['ir.ui.view'].with_context(active_test=False, _force_unlink=True).search(website_domain).unlink()
+            self.env['ir.ui.view'].with_context(active_test=False, force_delete=True).search(website_domain).unlink()
             self.env['ir.attachment'].with_context(active_test=False).search(website_domain).unlink()
         return super().unlink()
