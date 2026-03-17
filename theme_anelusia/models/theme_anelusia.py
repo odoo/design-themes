8b214decbc9bbe2364c4d96658c695eb32611f78
Chrysanthe (chgo) <chgo@odoo.com>
2026-03-17 18:24:08 +0100

    [IMP] theme_anelusia: redesign
    This commit fine-tunes the snippet sequence as well as all the snippets
    composing it in order to provide a better visual result and provide
    a real value to the theme (bento grid design).
    
    task-6036480

--- theme_anelusia/models/theme_anelusia.py
+++ theme_anelusia/models/theme_anelusia.py
@@ -6,3 +6,17 @@ class ThemeUtils(models.AbstractModel):
 
     def _theme_anelusia_post_copy(self, mod):
         self.enable_view('website.template_footer_headline')
+
+        if self.env['ir.module.module']._installed().get('website_sale'):
+            self.enable_view('website.template_header_sales_four')
+            self.env['website.assets'].make_scss_customization(
+                '/website/static/src/scss/options/user_values.scss',
+                {'header-template': 'sales_four'}
+            )
+        else:
+            self.enable_view('website.template_header_hamburger')
+            self.enable_view('website.no_autohide_menu')
+
+        # Set the header content width to full.
+        self.enable_view('website.header_width_full')
+
