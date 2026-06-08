4547d769ce1f5182142ce120d8e7815914e5d1c2
Antoine (anso) <anso@odoo.com>
2026-06-08 09:57:22 +0200

    [IMP] theme_kea: improve Kea Theme
    This commit freshens the design of the theme, switches it to an
    electronic devices e-commerce with glass and glow design.
    
    task-6065733
    
    closes odoo/design-themes#1306
    
    Related: odoo/odoo#273756
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- theme_kea/models/theme_kea.py
+++ theme_kea/models/theme_kea.py
@@ -5,4 +5,7 @@ class ThemeUtils(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_kea_post_copy(self, mod):
-        self.enable_view('website.template_footer_minimalist')
+        self.disable_view('website.header_call_to_action')
+
+        self.enable_view('website.template_header_sales_one')
+        self.enable_view('website.template_footer_headline')
