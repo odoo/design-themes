1f1df433a9e32c59625858bce9a088195ce62f3d
Chrysanthe (chgo) <chgo@odoo.com>
2026-07-10 09:10:05 +0200

    [IMP] theme_graphene: redesign
    task-6377094

--- theme_graphene/models/theme_graphene.py
+++ theme_graphene/models/theme_graphene.py
@@ -5,9 +5,5 @@ class ThemeUtils(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_graphene_post_copy(self, mod):
-        self.enable_view('website.template_header_stretch')
-
-        self.enable_view('website.template_footer_centered')
-
-        self.enable_asset("website.ripple_effect_scss")
-        self.enable_asset("website.ripple_effect_js")
+        self.enable_view('website.template_footer_links')
+        self.enable_view('website.footer_no_copyright')
--- theme_graphene/static/src/js/tour.js
+++ theme_graphene/static/src/js/tour.js
@@ -4,51 +4,57 @@ import * as wTourUtils from "@website/js/tours/tour_utils";
 
 const snippets = [
     {
-        id: 's_cover',
-        name: 'Cover',
+        id: 's_hero_minimalist',
+        name: 'Hero Minimalist',
         groupName: "Intro",
     },
     {
-        id: 's_text_image',
-        name: 'Text - Image',
+        id: 's_text_block_split',
+        name: 'Text Split',
+        groupName: "Text",
+    },
+    {
+        id: 's_services_grid',
+        name: 'Services Grid',
+        groupName: "Columns",
+    },
+    {
+        id: 's_showcase_square',
+        name: 'Showcase Square',
         groupName: "Content",
     },
     {
-        id: 's_numbers_grid',
-        name: 'Numbers Grid',
-        groupName: "Content",
+        id: 's_references_tiles',
+        name: 'References Tiles',
+        groupName: "People",
     },
     {
-        id: 's_mockup_image',
-        name: 'Mockup Image',
-        groupName: "Content",
+        id: 's_parallax',
+        name: 'Parallax',
+        groupName: "Images",
     },
     {
-        id: 's_comparisons',
-        name: 'Comparisons',
-        groupName: "Content",
-    },
-    {
-        id: 's_references',
-        name: 'References',
-        groupName: "Content",
+        id: 's_faq_collapse_centered',
+        name: 'FAQ Centered',
+        groupName: "Text",
     },
 ];
 
 
 wTourUtils.registerThemeHomepageTour("graphene_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"base-1"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"graphene"'),
     ...wTourUtils.insertSnippet(snippets[0]),
-    ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
-    wTourUtils.goBackToBlocks('left'),
+    ...wTourUtils.clickOnText(snippets[0], 'h1'),
+    wTourUtils.goBackToBlocks(),
 
     ...wTourUtils.insertSnippet(snippets[1]),
-
     ...wTourUtils.insertSnippet(snippets[2]),
-    ...wTourUtils.insertSnippet(snippets[3], { position: "top" }),
+    ...wTourUtils.insertSnippet(snippets[3]),
+    ...wTourUtils.insertSnippet(snippets[4]),
+    ...wTourUtils.insertSnippet(snippets[5]),
+    ...wTourUtils.insertSnippet(snippets[6]),
 
-    ...wTourUtils.insertSnippet(snippets[4], { position: "top" }),
     ...wTourUtils.clickOnSnippet(snippets[4], 'top'),
-    wTourUtils.changeBackgroundColor('left'),
+    wTourUtils.changeBackgroundColor(),
     wTourUtils.selectColorPalette(),
 ]);
