5adb0b06526db30f18d6ea7460cb75825a8948f5
Antoine (anso) <anso@odoo.com>
2026-07-13 16:22:59 +0200

    [IMP] theme_orchid: fine tune snippet sequence and colors
    task-6374308

--- theme_orchid/models/theme_orchid.py
+++ theme_orchid/models/theme_orchid.py
@@ -6,4 +6,4 @@ class ThemeUtils(models.AbstractModel):
 
     def _theme_orchid_post_copy(self, mod):
         self.enable_view('website.template_header_default')
-        self.enable_view('website.template_footer_descriptive')
+        self.enable_view('website.template_footer_minimalist')
