6790b13d45c56b68fd0f3bd2b5543c4cd48345fb
Antoine (anso) <anso@odoo.com>
2026-07-13 16:19:48 +0200

    [IMP] theme_real_estate: adapt hero design and snippet sequence
    task-6374308
    
    Part-of: odoo/design-themes#1321
    Related: odoo/odoo#278354
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- theme_real_estate/models/theme_real_estate.py
+++ theme_real_estate/models/theme_real_estate.py
@@ -7,3 +7,6 @@ class ThemeUtils(models.AbstractModel):
     def _theme_real_estate_post_copy(self, mod):
         self.enable_asset("website.ripple_effect_scss")
         self.enable_asset("website.ripple_effect_js")
+        self.enable_view('website.template_header_boxed')
+        self.set_page_option('header_overlay', True)
+        self.disable_view('website.header_call_to_action')
