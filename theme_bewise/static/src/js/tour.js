bdd25fbd1f5ab3ba441f0cf1e5928ec88d517f15
Antoine (anso) <anso@odoo.com>
2026-07-13 16:23:25 +0200

    [IMP] theme_bewise: refine theme look and feel
    Replace intro and change font + palette
    
    Add cta's in the second part of the homepage
    
    task-6374308

--- theme_bewise/static/src/js/tour.js
+++ theme_bewise/static/src/js/tour.js
@@ -3,8 +3,8 @@ import * as wTourUtils from '@website/js/tours/tour_utils';
 
 const snippets = [
     {
-        id: 's_striped_center_top',
-        name: 'Striped Center Top',
+        id: 's_intro_spotlight',
+        name: 'Intro Spotlight',
         groupName: "Intro",
     },
     {
@@ -35,7 +35,7 @@ const snippets = [
 ];
 
 wTourUtils.registerThemeHomepageTour("bewise_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"default-19"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-7"'),
     ...wTourUtils.insertSnippet(snippets[0], { position: "top" }),
     ...wTourUtils.clickOnText(snippets[0], 'h1', 'top'),
     wTourUtils.goBackToBlocks(),
