b540ca15d59e2d3c5513e3ee7dcc952d50d27cc2
Antoine (anso) <anso@odoo.com>
2026-07-15 15:10:38 +0200

    [IMP] theme_notes: redesign the theme
    This commit redesign theme_notes. Since "Music Band" is not an industry,
    we will switch the focus to "Opera house/Live music venue".
    
    task-6388674

--- theme_notes/models/theme_notes.py
+++ theme_notes/models/theme_notes.py
@@ -5,4 +5,7 @@ class ThemeUtils(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_notes_post_copy(self, mod):
-        self.enable_view('website.template_footer_descriptive')
+        self.enable_view('website.template_header_hamburger')
+        self.enable_view('website.no_autohide_menu')
+        self.enable_view('website.header_width_full')
+        self.enable_view('website.template_footer_headline')
