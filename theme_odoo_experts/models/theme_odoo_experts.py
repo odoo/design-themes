093849e2f8ca3941cfe43f43cd59c35b9f69292c
Chrysanthe (chgo) <chgo@odoo.com>
2026-07-16 10:54:46 +0200

    [IMP] theme_(clean,odoo_experts): change footer template
    Replace the footer template in these two themes for better ones.
    
    task-6374308
    
    Part-of: odoo/design-themes#1321
    Related: odoo/odoo#278354
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- theme_odoo_experts/models/theme_odoo_experts.py
+++ theme_odoo_experts/models/theme_odoo_experts.py
@@ -5,4 +5,4 @@ class ThemeUtils(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_odoo_experts_post_copy(self, mod):
-        self.enable_view('website.template_footer_contact')
+        return
